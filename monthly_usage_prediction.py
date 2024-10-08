6a48f6eded18014172608da62a004e398c9fdece
abduqayum.rasulmuhamedov <abduqayum.rasulmuhamedov@ssd.uz>
2024-10-08 23:26:58 +0500

    monthly prediction

---
+++ monthly_usage_prediction.py
@@ -0,0 +1,23 @@
+import prophet
+import pandas as pd
+import joblib
+
+monthly_water_usage_model = joblib.load("mothly_water_usage_model.joblib")
+
+ds = monthly_water_usage_model.make_future_dataframe(periods=30)
+
+prediction_df = monthly_water_usage_model.predict(ds)
+
+months = list(range(1, 13))
+
+month_list = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+year = 2020
+previous_month = "september"
+month_index = month_list.index(previous_month) + 1
+
+pred_for_october = prediction_df[(prediction_df['ds'].dt.year == year) & (prediction_df['ds'].dt.month == month_index)]["yhat"]
+next_month_water_usage = pred_for_october.sum()
+print(next_month_water_usage, "Liters")
+
+# print(prediction_df.head(30)[["ds", "yhat"]])
+
